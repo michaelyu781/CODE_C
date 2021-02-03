061943a666d687018420936bdd48d6706960de66
michael <michaelyu1101@163.com>
2021-02-03 22:47:16 +0800

    tic-tac-toe modified

--- tic-tac-toe/main.c
+++ tic-tac-toe/main.c
@@ -15,6 +15,23 @@ char *create_board()                                    //create a blank board
     return b;
 }
 
+//another way to judge who win the game:by comparing whether the characters are the same.
+int winner_judge(int (*board)[3])
+{
+    for(int i=0;i<3;i++)
+    {
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+            return 1;
+        else if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            return 1;
+        else if(board[0][0]==board[1][1]&&board[0][0]==board[2][2])
+            return 1;
+        else if(board[2][0]==board[1][1]&&board[2][0]==board[0][2])
+            return 1;
+    }
+    return 0;
+}
+
 void print_board(char(*b)[3])                           //print board
 {
     for(int i=0;i<4;i++)
@@ -83,13 +100,21 @@ int is_player2_win(char (*b)[3])
     return 0;
 }
 
+int is_draw(char(*board)[3])                                  //this is used to judge whether the game is over
+{
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            if(board[i][j]==' ') return 0;
+    return 1;
+}
+
 int main()
 {
     int i=1;                                                  //i is used to judge who's turn
     void (*p)(char,int,char(*)[3]);                           //p is used to judge palce which chess
     char (*board)[3]=create_board();
     print_board(board);
-    while(1)
+    while(is_draw(board))
     {
         char letter;int number;
         if(i%2)
@@ -104,7 +129,8 @@ int main()
         }
         start:scanf("%c%d",&letter,&number);
         fflush(stdin);
-        if(letter<'A'||letter>'C'||number<1||number>3) {printf("wrong!input again!");goto start;}
+        if(letter<'A'||letter>'C'||number<1||number>3) {printf("wrong!input again:");goto start;}
+        if(board[letter-'A'][number-1]!=' ') {printf("space occupied!input again:");goto start;}
         p(letter,number,board);
         print_board(board);
         i++;
