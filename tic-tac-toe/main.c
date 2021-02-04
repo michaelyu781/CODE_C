7482210b1f282a589921a083347193dd8f5b9cf5
michael <michaelyu1101@163.com>
2021-02-04 22:35:45 +0800

    modify function:is_draw

--- tic-tac-toe/main.c
+++ tic-tac-toe/main.c
@@ -104,8 +104,8 @@ int is_draw(char(*board)[3])                                  //this is used to 
 {
     for(int i=0;i<3;i++)
         for(int j=0;j<3;j++)
-            if(board[i][j]==' ') return 0;
-    return 1;
+            if(board[i][j]==' ') return 1;
+    return 0;
 }
 
 int main()
